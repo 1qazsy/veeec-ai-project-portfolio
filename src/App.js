c454d4ee447af1047d7dd447f71a70a39d31ff09
zhangshengyue1015 <zsy96115@163.com>
2024-07-01 21:43:59 +0800

    Feat

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import React, { useEffect } from "react"
 export default (() => {
     useEffect(() => {
         if (window.VecAICaptcha) {
-            if (!window.VecAICaptcha['myOneTexxxst']) {
-                window.VecAICaptcha({
-                    onSuccess: (data) => {},
-                    onClose: (data) => {},
-                    dom: 'myOneTexxxst', // 自定义挂载元素： 传入ID即可，不需要携带"#" 
-                });
-            }
+
+            window.VecAICaptcha({
+                onSuccess: (data) => { },
+                onClose: (data) => { },
+                dom: 'myOneTexxxst', // 自定义挂载元素： 传入ID即可，不需要携带"#" 
+            });
+
         }
     }, [])
 
